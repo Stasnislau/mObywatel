9107aa52f3d625f3d6fe282e52ad8d3c8293cfb2
nkvch <nkvch.st@gmail.com>
2023-10-22 05:30:24 +0200

    clear btn 2

--- src/components/wrapper/index.tsx
+++ src/components/wrapper/index.tsx
@@ -1,38 +1,18 @@
-import { observer } from "mobx-react-lite";
-import { Context } from "../../App.tsx";
-import { CircularProgress, Box } from "@mui/material";
-import React, { useContext } from "react";
 import { ClearAll } from "@mui/icons-material";
+import { observer } from "mobx-react-lite";
+import React from "react";
+import useStateLS from "../../hooks/useStateLS";
+import { chat } from "../../types";
 const WrapperComponent = observer(
   ({ children }: { children: React.ReactNode }) => {
-    const store = useContext(Context);
+    const [chat, setChat] = useStateLS<chat>("chat", { messages: [] });
     return (
       <div className="w-full h-screen flex flex-col">
         <header className="h-[10vh] flex justify-between items-center px-5">
           <h3 className="w-fit">mEkspert</h3>
-          <ClearAll />
+          <ClearAll onClick={() => setChat({ messages: [] })} />
         </header>
         {children}
-        <div
-          style={{
-            position: "fixed",
-            zIndex: 5000,
-          }}
-        >
-          {store.state.isLoading && (
-            <Box
-              sx={{
-                position: "fixed",
-                zIndex: 5000,
-                top: "90%",
-                left: "90%",
-                transform: "translate(-50%, -50%)",
-              }}
-            >
-              <CircularProgress />
-            </Box>
-          )}
-        </div>
       </div>
     );
   }
