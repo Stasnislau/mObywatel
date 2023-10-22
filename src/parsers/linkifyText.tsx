5185d88bed688958d9f93496c9001e84ac6a0351
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-10-22 05:14:31 +0200

    fixed brace

--- src/parsers/linkifyText.tsx
+++ src/parsers/linkifyText.tsx
@@ -14,11 +14,11 @@ function linkifyText(text: string): ReactNode {
     {
       textPieces.map((piece, index) => {
         if (piece.match(urlRegex)) {
-          const url = piece.match(/https?:\/\/[^\s]+/g);
+          const url = piece.match(/\(https?:\/\/[^\s]+\)/g);
           const text = piece.match(/\[.+\]/g);
           if (url && text) {
             return (
-              <a href={url[0]} target="_blank" rel="noreferrer" key={index}>
+              <a href={url[0].substring(1, url[0].length - 1)} target="_blank" rel="noreferrer" key={index}>
                 {text[0].substring(1, text[0].length - 1)}
               </a>
             );
